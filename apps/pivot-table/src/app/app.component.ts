2db96b7b33e5455c465f7bf42a3721d37d09fca9
vorant94 <vorant94@gmail.com>
2021-02-05 13:04:51 +0200

    refactored styles to pass prod build limits

--- apps/pivot-table/src/app/app.component.ts
+++ apps/pivot-table/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'ha-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'pivot-table';
--- apps/pivot-table/src/app/table/table.component.ts
+++ apps/pivot-table/src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TableService } from './table.service';
 import { SectionDto } from './dtos/section.dto';
 import { KeyValue } from '@angular/common';
@@ -9,7 +9,8 @@ import { CoordsModel } from './models/coords.model';
 @Component({
   selector: 'ha-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TableComponent implements OnInit {
 
